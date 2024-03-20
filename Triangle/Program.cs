1eb736ee3f9da76257dceb257c9a4d0a661002c8
Aslı Savaş <72549961+aslihansavas@users.noreply.github.com>
2024-03-21 00:40:40 +0300

    Add files via upload

---
+++ Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Triangle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Üçgenin boyutunu girin: ");
+        int size;
+
+        while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+        {
+            Console.WriteLine("Geçersiz giriş. Lütfen pozitif bir tam sayı girin.");
+            Console.Write("Üçgenin boyutunu girin: ");
+        }
+
+        TriangleDrawer.DrawTriangle(size);
+    }
+}
+class TriangleDrawer
+{
+    public static void DrawTriangle(int size)
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
