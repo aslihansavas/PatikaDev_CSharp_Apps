a665c85e608b05a28fa18fcc2db9ce266cfb2cd7
Aslı Savaş <72549961+aslihansavas@users.noreply.github.com>
2024-03-21 01:51:13 +0300

    Add files via upload

---
+++ SessizHarf/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SessizHarf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir string ifade girin:");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            Console.WriteLine("Sonuç:");
+
+            foreach (var word in words)
+            {
+                Console.Write(HasConsecutiveConsonants(word) + " ");
+            }
+        }
+
+        static bool HasConsecutiveConsonants(string word)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                char currentChar = word[i];
+                char nextChar = word[i + 1];
+
+                if (IsConsonant(currentChar) && IsConsonant(nextChar))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool IsConsonant(char c)
+        {
+            // Sessiz harfleri kontrol et
+            return !("aeiouAEIOU".Contains(c));
+        }
+    }
+}
